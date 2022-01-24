5f4e036a344ad2c90096c4f0d3beaf97a2e87715
Marc Cavazos <marcavazos19@icloud.com>
2022-01-24 11:35:50 -0500

    commit tree 9996150f3c0291bd9fa9dcdbc4d0246a513ee166

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+
+  return word.split('').reverse().join('');
+
 }
 
+function isPalindrome(word) {
+  // Write your algorithm here
+  const reversedWord = reverse(word)
+
+  return word === reversedWord
+}
 /* 
   Add your pseudocode here
+  reverse takes in a string. the split method is called on the string
+  to make the string an array of which the elements are its individual letters.
+  the reverse method is then called on the array. the reversed array is then joined
+  using the join method. the function returns the reversed word
+
+  a second function takes in a word as an argument. it then calls the reverse function
+  and stores its returned word. it then checks if the returned word, which is reversed
+  is strictly equal to the original word.
 */
 
 /*
   Add written explanation of your solution here
+  the program has two funtions. one function takes in a word and reverses the order of
+  letters. the other function tests whether that is strictly equal to the original
+  word.
 */
 
 // You can run `node index.js` to view these console logs
